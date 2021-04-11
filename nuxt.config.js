de983bda315bb03c878af0c0e683dc8f0f0fcde6
noithatzip <longlv@v-auto.com.vn>
2021-04-11 21:07:36 +0700

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,42 @@
+export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'Dịch vụ bảo vệ uy tín tại Hà Nội - Công ty bảo vệ Diamondstar',
+    htmlAttrs: {
+      lang: 'vi',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Công ty TNHH liên doanh dịch vụ bảo vệ Diamondstar cung cấp bảo vệ uy tín, báo giá dịch vụ bảo vệ chuyên nghiệp theo giờ, sự kiện và dài hạn tại Hà Nội..' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/buefy
+    'nuxt-buefy',
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+}
